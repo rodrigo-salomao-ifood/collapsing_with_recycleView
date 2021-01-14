dd2b980b682424794db6c9d0ee06aca2de0b1630
Salomao <rodrigo.salomao@ifood.com.br>
2021-01-13 21:50:00 -0300

    Animating collapse

--- app/src/main/java/com/example/dummy/ExpandedFragment.kt
+++ app/src/main/java/com/example/dummy/ExpandedFragment.kt
@@ -4,14 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 
 class ExpandedFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_expanded, container, false)
+        val view = inflater.inflate(R.layout.fragment_expanded, container, false)
+        val tittle = view.findViewById<View>(R.id.expanded_title)
+        val expandableContainer = view.findViewById<LinearLayout>(R.id.expanded_container)
+        tittle.setOnClickListener {
+            expandableContainer.apply {
+                visibility = if (expandableContainer.isVisible) View.GONE else View.VISIBLE
+            }
+        }
+        return view
+    }
+
+    private fun showToast() {
+        Toast.makeText(requireContext(), " Click ", Toast.LENGTH_LONG).show()
     }
 }
